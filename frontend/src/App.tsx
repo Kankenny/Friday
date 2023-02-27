d2f782b5228ac4dd4df0cc5013c604fd1cd79c50
Kennette James Basco <kenji.maddela01@gmail.com>
2023-02-26 20:40:32 -0500

    Test for Jil

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div className="bg-red-500">Hello World jil </div>
+  return (
+    <div className="bg-red-500">
+      Hello World Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Quisquam id laboriosam, quaerat saepe hic odio velit ipsam expedita
+      consectetur. Exercitationem itaque nisi atque laudantium eos dolor
+      deleniti nostrum ut quaerat? Nam ratione qui, eum cupiditate nihil, sint
+      culpa, in ducimus atque doloribus praesentium corporis explicabo
+      consequatur facilis eveniet laboriosam quasi.
+    </div>
+  )
 }
 
 export default App
