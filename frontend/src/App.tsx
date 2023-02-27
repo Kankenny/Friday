37ef5bb5df977ef0b15c9ed7438624e62165d478
Ethan <emedin04@nyit.edu>
2023-02-27 14:22:05 -0500

    erased lorem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
 function App() {
   return (
     <div className="bg-red-500">
-      Hello World Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Quisquam id laboriosam, quaerat saepe hic odio velit ipsam expedita
-      consectetur. Exercitationem itaque nisi atque laudantium eos dolor
-      deleniti nostrum ut quaerat? Nam ratione qui, eum cupiditate nihil, sint
-      culpa, in ducimus atque doloribus praesentium corporis explicabo
-      consequatur facilis eveniet laboriosam quasi.
+      Hello World, this is a test
     </div>
   )
 }
