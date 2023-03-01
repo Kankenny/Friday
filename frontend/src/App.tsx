07e832531277cedeed9e7b3a961171e384994df9
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-03-01 17:05:59 -0500

    Implement the StyledInlut component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Card from "./components/ui/Card";
+import StyledInput from "./components/ui/StyledInput";
 
 function App() {
   return (
-    <div className="bg-red-500">
-      Hello World, this is a test
-      <Card text="Hello"></Card>
+    <div className="flex justify-center flex-col items-center space-y-4">
+      <h1>Hello World, this is a test</h1>
+      <div className="w-64">
+        <StyledInput label="Some Label" name="Some Label" type="text" />
+      </div>
     </div>
   );
 }
--- frontend/src/components/ui/StyledInput.tsx
+++ frontend/src/components/ui/StyledInput.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+type Props = {
+  label: string;
+  name: string;
+  type: "text" | "email" | "password";
+  placeholder?: string;
+};
 
-const StyledInput = () => {
-  return <div>StyledInput</div>;
+const StyledInput = ({
+  label,
+  name,
+  type = "text",
+  placeholder,
+  ...rest
+}: Props) => {
+  return (
+    <div className="relative z-0 mb-8">
+      <input
+        {...rest}
+        id={name}
+        type={type}
+        placeholder={placeholder}
+        className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-blue-500 border-gray-200"
+      />
+      <label
+        htmlFor={name}
+        className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500"
+      >
+        {label}
+      </label>
+    </div>
+  );
 };
 
 export default StyledInput;
