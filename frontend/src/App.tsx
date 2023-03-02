0025788d10d7d02b5f2966b0490a91c063c00fbb
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-02 00:00:56 -0500

    Increase width of the UI components
    For display purposes only

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,22 @@ import StyledInput from "./components/ui/StyledInput"
 function App() {
   return (
     <div className="flex justify-center flex-col items-center">
-      <div className="w-64 space-y-4">
+      <div className="w-96 space-y-4">
         <h1 className="text-2xl font-bold">StyledInput Component</h1>
         <StyledInput placeholder="Some Label" name="Some Label" type="text" />
       </div>
 
-      <div className="w-64 space-y-4">
+      <div className="w-96 space-y-4">
         <h1 className="text-2xl font-bold">Card Component</h1>
         <Card>Some Content</Card>
       </div>
 
-      <div className="w-64 space-y-4">
+      <div className="w-96 space-y-4">
         <h1 className="text-2xl font-bold">StyledButton Component</h1>
         {/* Put component here */}
       </div>
 
-      <div className="w-64 space-y-4">
+      <div className="w-96 space-y-4">
         <h1 className="text-2xl font-bold">StyledLink Component</h1>
         {/* Put component here */}
       </div>
