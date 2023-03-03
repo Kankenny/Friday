7779054d8c93f1db96593eb81e9b54cfce907884
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-03-03 16:42:33 -0500

    Add border to the ui components for display purposes

--- frontend/src/components/routes/Playground.tsx
+++ frontend/src/components/routes/Playground.tsx
@@ -7,17 +7,17 @@ import Accordion from "../ui/Accordion";
 const Playground = () => {
   return (
     <div className="flex justify-center flex-col items-center">
-      <div className="w-96 space-y-4">
+      <div className="w-96 space-y-4 border-b-2 border-b-black mb-10 p-5">
         <h1 className="text-2xl font-bold">StyledInput Component</h1>
         <StyledInput placeholder="Some Label" name="Some Label" type="text" />
       </div>
 
-      <div className="w-96 space-y-4">
+      <div className="w-96 space-y-4 border-b-2 border-b-black mb-10 p-5">
         <h1 className="text-2xl font-bold">Card Component</h1>
         <Card>Some Content</Card>
       </div>
 
-      <div className="w-96 space-y-4">
+      <div className="w-96 space-y-4 border-b-2 border-b-black mb-10 p-5">
         <h1 className="text-2xl font-bold">StyledButton Component</h1>
         <div className="flex space-x-10">
           <StyledButton
@@ -33,12 +33,12 @@ const Playground = () => {
         </div>
       </div>
 
-      <div className="w-96 space-y-4">
+      <div className="w-96 space-y-4 border-b-2 border-b-black mb-10 p-5">
         <h1 className="text-2xl font-bold">StyledLink Component</h1>
         {/* Put component here */}
       </div>
 
-      <div className="w-96 space-y-4">
+      <div className="w-96 space-y-4 border-b-2 border-b-black mb-10 p-5">
         <h1 className="text-2xl font-bold">Accordion Component</h1>
         <Accordion title="Some Title">
           <h1>Some Heading</h1>
