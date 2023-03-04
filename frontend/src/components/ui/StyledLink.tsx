c11cdfda58e6f99958024e9b996978767f137c8e
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-04 11:24:42 -0500

    Add stylings to the StyledLink component

--- frontend/src/components/ui/StyledLink.tsx
+++ frontend/src/components/ui/StyledLink.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React from "react"
 
 type Props = {
-  href: string;
-  linkText: string;
-};
+  href: string
+  linkText: string
+}
 
 const StyledLink = ({ href, linkText }: Props) => {
-  return <a href="https://mddlknntt.monday.com/boards/4007749836">Our Monday board</a>;
-};
+  return (
+    <a
+      href="https://mddlknntt.monday.com/boards/4007749836"
+      className="underline text-tertiary hover:duration-200 hover:text-secondary"
+    >
+      Our Monday board
+    </a>
+  )
+}
 
-export default StyledLink;
+export default StyledLink
