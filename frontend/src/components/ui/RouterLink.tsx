5b64dacd0bf89da6a3e02178654f55669d28b32e
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-04 13:00:39 -0500

    Add add-on stylings to the RouterLink component
    The add-on stylings to the said component will be appended to the default styles when its path is active

--- frontend/src/components/ui/RouterLink.tsx
+++ frontend/src/components/ui/RouterLink.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 type Props = {
   to: string
@@ -7,10 +7,15 @@ type Props = {
 }
 
 const RouterLink = ({ to, routerLinkText, twClasses }: Props) => {
+  const { pathname } = useLocation()
+  console.log(pathname)
+
+  const activatedRouterLinkClasses = pathname === to && "text-tertiary"
+
   return (
     <Link
       to={to}
-      className={`text-lg font-bold text-secondary hover:text-tertiary duration-100 ${twClasses}`}
+      className={`text-lg font-bold text-secondary hover:text-tertiary duration-100 ${activatedRouterLinkClasses} ${twClasses}`}
     >
       {routerLinkText}
     </Link>
