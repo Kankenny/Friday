ffb8b2298bcb7b54386de4c5e8a6499f9aea01c8
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-04 11:34:00 -0500

    Improve semantics of the Home Route into the App Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 
-import LandingPage from "./components/routes/LandingPage";
-import Home from "./components/routes/Home";
-import Playground from "./components/routes/Playground";
-import RouterLink from "./components/ui/RouterLink";
+import LandingPage from "./components/routes/LandingPage"
+import Home from "./components/routes/Home"
+import Playground from "./components/routes/Playground"
+import RouterLink from "./components/ui/RouterLink"
 
 function App() {
   return (
     <>
       <RouterLink to="/" routerLinkText="Landing Page" />
-      <RouterLink to="/home" routerLinkText="Home" />
+      <RouterLink to="/app" routerLinkText="App" />
       <RouterLink to="/playground" routerLinkText="Playground" />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/app" element={<Home />} />
         <Route path="/playground" element={<Playground />} />
       </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
