f2c38caaa4eba6cf5cea34c51dfe5bb94b76a354
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-04 11:36:46 -0500

    Implement the Overview component

---
+++ frontend/src/components/ui/Overview.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+type Props = {
+  children: React.ReactNode
+  twClasses?: string
+}
+
+const Overview = ({ children, twClasses }: Props) => {
+  return (
+    <div className={`flex flex-col bg-main text-secondary ${twClasses}`}>
+      {children}
+    </div>
+  )
+}
+
+export default Overview
