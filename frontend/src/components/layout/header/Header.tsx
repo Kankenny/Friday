4aae29f128c44807fce5979ec45dfbeaac1d9a97
THossen <tan.hossen.1@gmail.com>
2023-03-09 16:21:43 -0500

    added header

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -1,5 +1,18 @@
+import RouterLink from "../../ui/RouterLink"
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+  <header className = "bg-main flex justify-between items-center">
+    <nav> 
+    <div className="space-x-4">
+        <RouterLink to="/" routerLinkText="Landing Page" />
+        <RouterLink to="/app" routerLinkText="App" />
+        <RouterLink to="/404Error" routerLinkText="404 Page" />
+        <RouterLink to="/playground" routerLinkText="Playground" />
+      </div>
+    </nav>
+  </header>
+  )
 }
 
 export default Header
