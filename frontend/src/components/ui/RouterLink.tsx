d6f3204d438363586f0b9d7af7cc07ec26571d6b
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-03-10 15:57:31 -0500

    Improve styling of RouterLink component on hover:

--- frontend/src/components/ui/RouterLink.tsx
+++ frontend/src/components/ui/RouterLink.tsx
@@ -1,24 +1,24 @@
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
 type Props = {
-  to: string
-  routerLinkText: string
-  twClasses?: string
-}
+  to: string;
+  routerLinkText: string;
+  twClasses?: string;
+};
 
 const RouterLink = ({ to, routerLinkText, twClasses }: Props) => {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
-  const activatedRouterLinkClasses = pathname === to && "text-tertiary"
+  const activatedRouterLinkClasses = pathname === to && "text-tertiary";
 
   return (
     <Link
       to={to}
-      className={`text-lg font-bold text-main hover:text-tertiary duration-100 ${activatedRouterLinkClasses} ${twClasses}`}
+      className={`text-lg font-bold text-main hover:text-tertiary ease-in-out duration-200 ${activatedRouterLinkClasses} ${twClasses}`}
     >
       {routerLinkText}
     </Link>
-  )
-}
+  );
+};
 
-export default RouterLink
+export default RouterLink;
