ef90d31b7a1d2bf04a812a304a5aad9466925430
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-17 23:43:45 -0400

    Improve color scheme of the Accordion component

--- frontend/src/components/routes/landing-page/FAQ.tsx
+++ frontend/src/components/routes/landing-page/FAQ.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import Accordion from "../../ui/Accordion"
 
 const FAQ = () => {
-  return <div>FAQ</div>;
-};
+  return (
+    <div className="container mx-auto max-w-[30%] flex flex-col space-y-5">
+      <Accordion title="What makes application your different from traditional to do applications?">
+        <p>
+          Our application allows our users to be able to prioritize their tasks
+          based on several factors such as the deadline, or you can even set it
+          the priority value yourself!
+        </p>
+      </Accordion>
+      <Accordion title="Who can see my to do lists?">
+        <p>
+          You can control who can access your to do lists. May it be your
+          friends, only specific people, or only private to you
+        </p>
+      </Accordion>
+      <Accordion title="What makes your different from traditional to do applications?">
+        <p>
+          Our application allows our users to be able to prioritize their tasks
+          based on several factors such as the deadline or you can set it the
+          priority value yourself!
+        </p>
+      </Accordion>
+      <Accordion title="What makes your different from traditional to do applications?">
+        <p>
+          Our application allows our users to be able to prioritize their tasks
+          based on several factors such as the deadline or you can set it the
+          priority value yourself!
+        </p>
+      </Accordion>
+    </div>
+  )
+}
 
-export default FAQ;
+export default FAQ
--- frontend/src/components/ui/Accordion.tsx
+++ frontend/src/components/ui/Accordion.tsx
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 type Props = {
-  title: string;
-  children: React.ReactNode;
-};
+  title: string
+  children: React.ReactNode
+}
 
 const Accordion = ({ title, children }: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openAccordionClasses =
+    isOpen && "hover:text-tertiary duration-300 ease-in-out"
 
   const toggleAccordion = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   return (
-    <div className="border border-secondary rounded-md overflow-hidden mb-4">
-      <div
-        className="bg-main px-4 py-3 flex justify-between items-center cursor-pointer"
-        onClick={toggleAccordion}
-      >
+    <div
+      className={`group bg-secondary text-main border border-secondary border-opacity-30 rounded-md overflow-hidden p-2 cursor-pointer ${openAccordionClasses}`}
+      onClick={toggleAccordion}
+    >
+      <div className="px-4 py-3 flex justify-between items-center ">
         <h2 className="text-lg font-medium">{title}</h2>
         <svg
-          className={`${
-            isOpen ? "transform rotate-180" : ""
-          } h-7 w-7 text-secondary  duration-300 ease-in-out`}
+          className={`${isOpen ? "transform rotate-180" : ""} h-7 w-7`}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
@@ -45,7 +46,7 @@ const Accordion = ({ title, children }: Props) => {
         <div className="px-4 py-3">{children}</div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Accordion;
+export default Accordion
