68b6907ee91af98c189fc85cfb171d4ad70cd618
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-25 16:09:08 -0400

    Fix dimensions of the Let's Get Started RouterLink component

--- frontend/src/components/routes/landing-page/Headline.tsx
+++ frontend/src/components/routes/landing-page/Headline.tsx
@@ -13,19 +13,19 @@ const Headline = () => {
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-center mx-auto h-full text-center md:text-left">
-      <div className="bg-white p-16 ">
-        <h1 className="text-tertiary text-6xl font-bold mb-8">
+      <div className="bg-white p-16 space-y-10">
+        <h1 className="text-tertiary text-7xl font-bold">
           {head}
           {dayOfWeek}
         </h1>
-        <p className="text-gray-500 text-xl mb-10">{subhead}</p>
+        <p className="text-gray-500 text-xl">{subhead}</p>
         <RouterLink
           routerLinkText="Let's Get Started"
           to="/app"
-          twClasses="bg-secondary px-10 py-4 rounded-md text-3xl"
+          twClasses="block md:inline-block bg-secondary px-10 py-4 rounded-md text-3xl"
         />
       </div>
-      <div className="w-2/5">
+      <div className="w-2/5 min-w-max">
         <img src="/to-do.gif" alt="to-do gif" className="ml-auto" />
       </div>
     </div>
