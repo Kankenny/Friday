f725aab9daed03811e07e2a094a7500d9c5d473d
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-01 21:09:24 -0400

    feat: setup user model

---
+++ server/models/User.ts
@@ -0,0 +1,79 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  securityQuestion: {
+    type: String,
+    required: true,
+  },
+  securityAnswer: {
+    type: String,
+    required: true,
+  },
+  following: {
+    type: ObjectId,
+    ref: "User",
+  },
+  followers: {
+    type: ObjectId,
+    ref: "User",
+  },
+  blocked: {
+    type: ObjectId,
+    ref: "User",
+  },
+  posts: {
+    type: ObjectId,
+    ref: "Post",
+  },
+  upvotedPosts: {
+    type: ObjectId,
+    ref: "Post",
+  },
+  downvotedPosts: {
+    type: ObjectId,
+    ref: "Post",
+  },
+  savedPosts: {
+    type: ObjectId,
+    ref: "Post",
+  },
+  notifications: {
+    type: ObjectId,
+    ref: "Notification",
+  },
+  comments: {
+    type: ObjectId,
+    ref: "Comment",
+  },
+})
+
+const UserModel = mongoose.model("Users", UserSchema)
+
+export default UserModel
