0e098b389373ecf2b99136cee55394082fb66488
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-01 21:15:49 -0400

    feat: implement task model

---
+++ server/models/Task.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const TaskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  progress: {
+    type: String,
+    required: true,
+  },
+  postId: {
+    type: ObjectId,
+    ref: "Post",
+  },
+})
+
+const TaskModel = mongoose.model("Tasks", TaskSchema)
+
+export default TaskModel
