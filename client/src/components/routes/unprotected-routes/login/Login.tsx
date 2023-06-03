bb42297788c97206240be9441c78f9c3697f6949
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-03 11:22:48 -0400

    refactor: make sign in form width dynamic to the size of the viewport

--- client/src/components/routes/unprotected-routes/login/Login.tsx
+++ client/src/components/routes/unprotected-routes/login/Login.tsx
@@ -5,7 +5,7 @@ import LoginForm from "./LoginForm"
 const Login = () => {
   return (
     <Body>
-      <div className="min-h-screen flex flex-col justify-center p-20 container mx-auto">
+      <div className="min-h-screen flex flex-col justify-center p-10 w-3/4 lg:w-[50em] mx-auto">
         <Card twClasses="text-center pt-0">
           <h1 className="text-3xl font-semibold bg-secondary text-main p-5 rounded-t-md">
             Sign in
