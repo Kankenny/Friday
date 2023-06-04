e21fa7235db9da32a0d3c3e95db46e995a4331c1
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-04 16:59:06 -0400

    feat: implement get individual post controller

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -22,3 +22,28 @@ export const getPosts = async (req: Request, res: Response) => {
     res.status(500).json({ message: err, data: null, ok: false })
   }
 }
+
+export const getPost = async (req: Request, res: Response) => {
+  // Destructure payload from the request params
+  const { postId } = req.params
+
+  try {
+    const existingPost = await PostModel.find({ _id: postId })
+
+    // Check if post exists
+    if (!existingPost) {
+      return res
+        .status(404)
+        .json({ message: "Post not found!", data: null, ok: false })
+    }
+
+    res.status(200).json({
+      message: "Post successfully fetched!",
+      data: existingPost,
+      ok: true,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: err, data: null, ok: false })
+  }
+}
