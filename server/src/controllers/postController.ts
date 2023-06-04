bfa7e48a929e7ddfc59433c71bed8e988923e761
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-04 18:16:42 -0400

    feat: implement the delete post controller fn

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -180,6 +180,56 @@ export const updatePost = async (req: JWTRequest, res: Response) => {
   }
 }
 
+export const deletePost = async (req: JWTRequest, res: Response) => {
+  try {
+    // Destructure payload from the request params
+    const { postId } = req.params
+
+    // Extract decoded token from verifyToken middleware
+    const { _idFromToken } = req.user
+
+    // Check if user exists
+    const existingUser = await UserModel.findById(_idFromToken)
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Invalid Credentials!", data: null, ok: false })
+    }
+
+    // Check if post exists
+    const existingPost = await PostModel.findById(postId)
+    if (!existingPost) {
+      return res
+        .status(400)
+        .json({ message: "Post not found!", data: null, ok: false })
+    }
+
+    // Check if the user that is deleting the post is the owner
+    const isOwner = existingUser.username === existingPost.creatorUsername
+    if (!isOwner) {
+      return res
+        .status(400)
+        .json({ message: "Unauthorized request!", data: null, ok: false })
+    }
+
+    // Delete the post
+    await PostModel.deleteOne({ _id: postId })
+
+    res.status(200).json({
+      message: "Post successfully deleted!",
+      data: null,
+      ok: true,
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      message: `Failed to delete post!: ${err}`,
+      data: null,
+      ok: false,
+    })
+  }
+}
+
 export const authorizeUserToPost = async (req: JWTRequest, res: Response) => {
   try {
     // Destructure payload from the request params
