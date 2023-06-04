97c9da9aaaa82ce1e3adeef3cd49f5947f0adc08
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-04 17:32:05 -0400

    feat: implement update post controller fn

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express"
 import PostModel from "../models/Post"
+import UserModel from "../models/User"
+
+// Types
+import JWTRequest from "../lib/types/JWTRequestType"
 
 // Validators
 import createPostSchema from "../lib/validations/createPostValidator"
-import JWTRequest from "../lib/types/JWTRequestType"
-import UserModel from "../models/User"
+import updatePostSchema from "../lib/validations/updatePostValidator"
+import mongoose from "mongoose"
 
 export const getPosts = async (req: Request, res: Response) => {
   try {
@@ -111,6 +115,69 @@ export const createPost = async (req: JWTRequest, res: Response) => {
     res.status(200).json({
       message: "Post successfully created!",
       data: newPost,
+      ok: true,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: err, data: null, ok: false })
+  }
+}
+
+export const updatePost = async (req: JWTRequest, res: Response) => {
+  // Validate body using the update post schema
+  try {
+    updatePostSchema.parse(req.body)
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({
+      message: "Invalid update post data!",
+      data: null,
+      ok: false,
+    })
+  }
+
+  // Destructure payload from the request params
+  const { postId } = req.params
+
+  try {
+    // Extract decoded token from verifyToken middleware
+    const { _idFromToken } = req.user
+
+    // Check if user exists
+    const existingUser = await UserModel.findById({ _idFromToken })
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Invalid Credentials!", data: null, ok: false })
+    }
+
+    // Check if post exists
+    const existingPost = await PostModel.findById({ postId })
+    if (!existingPost) {
+      return res
+        .status(400)
+        .json({ message: "Post not found!", data: null, ok: false })
+    }
+
+    // Check if the user that is updating is the creator of the post
+    const isOwner = existingUser.username === existingPost.creatorUsername
+    if (!isOwner) {
+      return res
+        .status(400)
+        .json({ message: "Unauthorized request!", data: null, ok: false })
+    }
+
+    // Update post
+    const fieldsToBeUpdated = updatePostSchema.parse(req.body)
+    const updatedPost = await PostModel.findByIdAndUpdate(
+      postId,
+      { $set: { ...fieldsToBeUpdated } },
+      { new: true }
+    )
+
+    res.status(200).json({
+      message: "Post successfully updated!",
+      data: updatedPost,
       ok: true,
     })
   } catch (err) {
