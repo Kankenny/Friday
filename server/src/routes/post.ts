78dd95df2a726b855369df056c33722f41a95255
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-04 18:13:41 -0400

    feat: implement post routes

---
+++ server/src/routes/post.ts
@@ -0,0 +1,67 @@
+import express, { NextFunction, Request, Response } from "express"
+
+// Post Controller Functions
+import {
+  getPosts,
+  getPost,
+  updatePost,
+  authorizeUserToPost,
+  unauthorizeUserToPost,
+  upvotePost,
+  downvotePost,
+} from "../controllers/postController"
+
+// Types
+import JWTRequest from "../lib/types/JWTRequestType"
+import verifyToken from "../middlewares/verifyToken"
+
+// Router
+const PostRouter = express.Router()
+
+// GET ALL POSTS
+PostRouter.get("/", getPosts)
+
+// GET POST
+PostRouter.get("/:postId", getPost)
+
+// UPDATE POST
+PostRouter.put(
+  "/:postId",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => updatePost(req as JWTRequest, res)
+)
+
+// AUTHORIZE USER
+PostRouter.post(
+  "/:postId/authorize/:userIdToAuthorize",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => authorizeUserToPost(req as JWTRequest, res)
+)
+
+// UNAUTHORIZE USER
+PostRouter.post(
+  "/:postId/unauthorize/:userIdToUnauthorize",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => unauthorizeUserToPost(req as JWTRequest, res)
+)
+
+// UPVOTE POST
+PostRouter.post(
+  "/:postId/upvote",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => upvotePost(req as JWTRequest, res)
+)
+
+// DOWNVOTE POST
+PostRouter.post(
+  "/:postId/downvote",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => downvotePost(req as JWTRequest, res)
+)
+
+export default PostRouter
