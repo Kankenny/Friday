efad4093b01ea7ccba97db9d4bdd9700fbf65e94
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-04 16:24:58 -0400

    feat: implement get user controller

---
+++ server/src/routes/user.ts
@@ -0,0 +1,19 @@
+import express, { NextFunction, Request, Response } from "express"
+
+// Auth Controller Functions
+import { getUser } from "../controllers/userController"
+
+// Types
+import JWTRequest from "../lib/types/JWTRequestType"
+import verifyToken from "../middlewares/verifyToken"
+
+// Router
+const UserRoute = express.Router()
+
+// GET USER
+UserRoute.get(
+  "/:userId",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => getUser(req as JWTRequest, res)
+)
