189a7ae4476a108b979cc5f7836cbb799cd96ada
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-05 19:24:23 -0400

    refactor: explicitly define fields allowed and disallowed to be updated in a post

--- server/src/lib/validations/updatePostValidator.ts
+++ server/src/lib/validations/updatePostValidator.ts
@@ -1,9 +1,13 @@
 import { z } from "zod"
 
-// Disallow these fields to be updated on the update post controller
 const updatePostSchema = z.object({
+  title: z.string().optional(),
   creatorId: z.never(),
   creatorUsername: z.never(),
+  dueDate: z.date().optional(),
+  color: z.string().optional(),
+  category: z.string().optional(),
+  visibility: z.string().optional(),
   upvotes: z.never(),
   downvotes: z.never(),
   authorizedUsers: z.never(),
