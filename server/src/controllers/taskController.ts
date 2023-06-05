419533a9752476bfebe2f09e7b4a6f27a71d5941
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-05 19:43:10 -0400

    feat: implement change task progress controller fn

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -7,6 +7,7 @@ import TaskModel from "../models/Task"
 
 // Validators
 import createTaskSchema from "../lib/validations/createTaskValidator"
+import updateTaskProgressSchema from "../lib/validations/updateTaskProgressValidator"
 
 export const getPostTasks = async (req: Request, res: Response) => {
   // Extract postId from the request query
@@ -91,5 +92,70 @@ export const createTask = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error)
     return res.status(500).json({ message: error, data: null, ok: false })
+  }
+}
+
+export const changeTaskProgress = async (req: Request, res: Response) => {
+  try {
+    // Validate body using the update task progress schema
+    updateTaskProgressSchema.parse(req.body)
+
+    // Extract progress payload from the request body
+    const { progress } = req.body
+
+    // Check if post exists
+    const postId = req.query.postId
+    if (!postId) {
+      return res.status(400).json({
+        message: "Bad Request!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    const existingPost = await PostModel.findById(postId)
+    if (!existingPost) {
+      return res.status(404).json({
+        message: "Post not found!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Check if task exists
+    const taskId = req.query.taskId
+    if (!taskId) {
+      return res.status(400).json({
+        message: "Bad Request!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    const existingTask = await TaskModel.findById(taskId)
+    if (!existingTask) {
+      return res.status(404).json({
+        message: "Task not found!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Update task progress
+    existingTask.progress = progress
+    await existingTask.save()
+
+    res.status(200).json({
+      message: "Task progress updated successfully!",
+      data: existingTask,
+      ok: true,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: "Internal server error",
+      data: null,
+      ok: false,
+    })
   }
 }
