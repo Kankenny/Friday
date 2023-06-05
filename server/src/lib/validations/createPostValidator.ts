a63b8d83ecb15847b4d8134825f33198e80e9c41
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-05 19:31:23 -0400

    refactor: define explicit visibility values of posts

--- server/src/lib/validations/createPostValidator.ts
+++ server/src/lib/validations/createPostValidator.ts
@@ -1,13 +1,13 @@
 import { z } from "zod"
 
 const createPostSchema = z.object({
-  title: z.string().min(1),
+  title: z.string().min(1).trim(),
   creatorId: z.string().min(1),
   creatorUsername: z.string().min(1),
   dueDate: z.date().optional(),
   color: z.string().min(1),
   category: z.string().min(1),
-  visibility: z.string().min(1),
+  visibility: z.enum(["personal", "collaborators", "private", "public"]),
   authorization: z.string().min(1),
 })
 
--- server/src/lib/validations/updatePostValidator.ts
+++ server/src/lib/validations/updatePostValidator.ts
@@ -1,13 +1,13 @@
 import { z } from "zod"
 
 const updatePostSchema = z.object({
-  title: z.string().optional(),
+  title: z.string().trim().optional(),
   creatorId: z.never(),
   creatorUsername: z.never(),
   dueDate: z.date().optional(),
   color: z.string().optional(),
   category: z.string().optional(),
-  visibility: z.string().optional(),
+  visibility: z.enum(["personal", "collaborators", "private", "public"]),
   upvotes: z.never(),
   downvotes: z.never(),
   authorizedUsers: z.never(),
