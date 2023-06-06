a0afc30051ec349d5344a2b82bbca0590cc7917a
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-05 20:37:55 -0400

    feat: implement delete subtask controller fn

--- server/src/controllers/subtaskController.ts
+++ server/src/controllers/subtaskController.ts
@@ -212,3 +212,80 @@ export const changeSubtaskProgress = async (req: JWTRequest, res: Response) => {
     })
   }
 }
+
+export const deleteSubtask = async (req: JWTRequest, res: Response) => {
+  try {
+    // Extract subtaskId from the request query
+    const subtaskId = req.query.subtaskId
+    if (!subtaskId) {
+      return res.status(400).json({
+        message: "Bad Request!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Check if the subtask exists
+    const existingSubtask = await SubtaskModel.findById(subtaskId)
+    if (!existingSubtask) {
+      return res.status(404).json({
+        message: "Subtask not found!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Extract postId from the request query
+    const { postId, taskId } = req.query
+
+    // Check if post exists
+    const existingPost = await PostModel.findById(postId)
+    if (!existingPost) {
+      return res.status(404).json({
+        message: "Post not found!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Check if task exists
+    const existingTask = await TaskModel.findById(taskId)
+    if (!existingTask) {
+      return res.status(404).json({
+        message: "Task not found!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Check if the user who is deleting the subtask is authorized to do so
+    const { _idFromToken } = req.user
+    const objectId = new mongoose.Types.ObjectId(_idFromToken)
+    const isOwner = existingPost.creatorId!.equals(objectId)
+    const isCollaborator = existingPost.authorizedUsers.some((userId) =>
+      userId.equals(objectId)
+    )
+
+    if (!isOwner || !isCollaborator) {
+      return res
+        .status(400)
+        .json({ message: "Unauthorized request!", data: null, ok: false })
+    }
+
+    // Delete the subtask
+    await SubtaskModel.deleteOne({ _id: subtaskId })
+
+    res.status(200).json({
+      message: "Subtask deleted successfully!",
+      data: null,
+      ok: true,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: "Internal server error",
+      data: null,
+      ok: false,
+    })
+  }
+}
