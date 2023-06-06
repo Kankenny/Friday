2de28494598b3e4c8c0866fe65a3521aa7e3425b
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-06 17:12:44 -0400

    feat: declare and expose notification controller on API

---
+++ server/src/routes/notification.ts
@@ -0,0 +1,35 @@
+// Dependencies
+import express, { Request, Response, NextFunction } from "express"
+
+// Notifications Controller Functions
+import {
+  getUserNotifications,
+  visitUserNotifications,
+} from "../controllers/notification-controller/notificationsController"
+
+// Middlewares
+import verifyToken from "../middlewares/verifyToken"
+
+// Types
+import JWTRequest from "../lib/types/JWTRequestType"
+
+const NotificationRouter = express.Router()
+
+// GET USER NOTIFICATIONS
+NotificationRouter.get(
+  "/:userId",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => getUserNotifications(req as JWTRequest, res)
+)
+
+// VISIT USER NOTIFICATIONS
+NotificationRouter.put(
+  "/visit",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) =>
+    visitUserNotifications(req as JWTRequest, res)
+)
+NotificationRouter
+export default NotificationRouter
