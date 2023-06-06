2b312a50016e3f83b56cc389341dd0ad1d3e4a38
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-06 16:26:08 -0400

    refactor: modularize user controller

---
+++ server/src/controllers/user-controller/user-actions/getUserFollowers.ts
@@ -0,0 +1,44 @@
+// Dependencies
+import { Request, Response } from "express"
+import mongoose from "mongoose"
+
+// Models
+import UserModel from "../../../models/User"
+
+export const getUserFollowers = async (req: Request, res: Response) => {
+  // Extract username from params
+  const { userId } = req.params
+
+  // Check if appropriate payload is attached to the body
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ message: "userId params is required!", data: null, ok: false })
+  }
+
+  // Check if userId is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res
+      .status(400)
+      .json({ message: "Invalid userId!", data: null, ok: false })
+  }
+
+  try {
+    // Check if user exists
+    const existingUser = await UserModel.findById(userId).populate("followers")
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ message: "User not found!", data: null, ok: false })
+    }
+
+    res.status(200).json({
+      message: "User followers successfully fetched!",
+      data: existingUser.followers,
+      ok: true,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: err, data: null, ok: false })
+  }
+}
---
+++ server/src/controllers/user-controller/userController.ts
@@ -0,0 +1,20 @@
+// User Actions
+import { getUser } from "./user-actions/getUser"
+import { getUserFollowing } from "./user-actions/getUserFollowing"
+import { getUserFollowers } from "./user-actions/getUserFollowers"
+import { getUserBlocked } from "./user-actions/getUserBlocked"
+import { followUser } from "./user-actions/followUser"
+import { unfollowUser } from "./user-actions/unfollowUser"
+import { blockUser } from "./user-actions/blockUser"
+import { unblockUser } from "./user-actions/unblockUser"
+
+export {
+  getUser,
+  getUserFollowing,
+  getUserFollowers,
+  getUserBlocked,
+  followUser,
+  unfollowUser,
+  blockUser,
+  unblockUser,
+}
