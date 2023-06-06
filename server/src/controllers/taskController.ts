61012d26bc77d350fbdf82fdfbfbf6867f7a8297
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-05 20:02:48 -0400

    feat: implement delete task controller fn

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -84,7 +84,7 @@ export const createTask = async (req: JWTRequest, res: Response) => {
     // Check if the user who is creating the task is authorized to do so
     const { _idFromToken } = req.user
     const objectId = new mongoose.Types.ObjectId(_idFromToken)
-    const isOwner = existingPost.creatorId === objectId
+    const isOwner = existingPost.creatorId!.equals(objectId)
     const isCollaborator = existingPost.authorizedUsers.some((userId) =>
       userId.equals(objectId)
     )
@@ -161,7 +161,7 @@ export const changeTaskProgress = async (req: JWTRequest, res: Response) => {
     // Check if the user who is updating the task is authorized to do so
     const { _idFromToken } = req.user
     const objectId = new mongoose.Types.ObjectId(_idFromToken)
-    const isOwner = existingPost.creatorId === objectId
+    const isOwner = existingPost.creatorId!.equals(objectId)
     const isCollaborator = existingPost.authorizedUsers.some((userId) =>
       userId.equals(objectId)
     )
@@ -178,6 +178,71 @@ export const changeTaskProgress = async (req: JWTRequest, res: Response) => {
     res.status(200).json({
       message: "Task progress updated successfully!",
       data: existingTask,
+      ok: true,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: "Internal server error",
+      data: null,
+      ok: false,
+    })
+  }
+}
+
+export const deleteTask = async (req: JWTRequest, res: Response) => {
+  try {
+    // Extract taskId from the request query
+    const taskId = req.query.taskId
+    if (!taskId) {
+      return res.status(400).json({
+        message: "Bad Request!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Check if the task exists
+    const existingTask = await TaskModel.findById(taskId)
+    if (!existingTask) {
+      return res.status(404).json({
+        message: "Task not found!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    // Check if the user who is deleting the task is authorized to do so
+    const { _idFromToken } = req.user
+    const objectId = new mongoose.Types.ObjectId(_idFromToken)
+    const postId = existingTask.postId
+
+    const existingPost = await PostModel.findById(postId)
+    if (!existingPost) {
+      return res.status(404).json({
+        message: "Post not found!",
+        data: null,
+        ok: false,
+      })
+    }
+
+    const isOwner = existingPost.creatorId!.equals(objectId)
+    const isCollaborator = existingPost.authorizedUsers.some((userId) =>
+      userId.equals(objectId)
+    )
+
+    if (!isOwner || !isCollaborator) {
+      return res
+        .status(400)
+        .json({ message: "Unauthorized request!", data: null, ok: false })
+    }
+
+    // Delete the task
+    await TaskModel.deleteOne({ _id: taskId })
+
+    res.status(200).json({
+      message: "Task deleted successfully!",
+      data: null,
       ok: true,
     })
   } catch (error) {
