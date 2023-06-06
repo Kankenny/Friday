fdb1c1e1e7c810f3bfe1704faecb3045a883cc64
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-06 17:09:34 -0400

    feat: declare and expose subtask controller on API

---
+++ server/src/routes/subtask.ts
@@ -0,0 +1,47 @@
+// Dependencies
+import express, { Request, Response, NextFunction } from "express"
+
+// Task Controller Functions
+import {
+  changeSubtaskProgress,
+  createSubtask,
+  deleteSubtask,
+  getTaskSubtasks,
+} from "../controllers/subtask-controller/subtaskController"
+
+// Middlewares
+import verifyToken from "../middlewares/verifyToken"
+
+// Types
+import JWTRequest from "../lib/types/JWTRequestType"
+
+const SubtaskRouter = express.Router()
+
+// GET TASK SUBTASKS
+SubtaskRouter.get("/getTaskSubtasks", getTaskSubtasks)
+
+// CREATE SUBTASK
+SubtaskRouter.post(
+  "/createSubtask",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => createSubtask(req as JWTRequest, res)
+)
+
+// DELETE TASK
+SubtaskRouter.delete(
+  "/deleteSubtask",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => deleteSubtask(req as JWTRequest, res)
+)
+
+// CHANGE TASK PROGRESS
+SubtaskRouter.post(
+  "/changeSubtaskProgress",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => changeSubtaskProgress(req as JWTRequest, res)
+)
+
+export default SubtaskRouter
