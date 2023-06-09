90ca760d7b72dad9b59054aa381708fd3b776dde
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-09 19:40:15 -0400

    refactor: utilize redux store

--- client/src/lib/hooks/redux-hook/useRedux.ts
+++ client/src/lib/hooks/redux-hook/useRedux.ts
@@ -1,6 +1,5 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import type { TypedUseSelectorHook } from "react-redux"
-import { AppDispatch, RootState } from "../../store/store"
+import { RootState } from "../../store/store"
 
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector
