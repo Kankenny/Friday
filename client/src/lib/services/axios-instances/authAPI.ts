792d7150b00820ea410b5cfe42ef964f6e1172f7
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-09 22:37:09 -0400

    feat: implement authAPI axios instance

---
+++ client/src/lib/services/axios-instances/authAPI.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const authAPI = axios.create({
+  baseURL: `http://localhost:${
+    import.meta.env.VITE_BACKEND_SERVER_PORT
+  }/api/auth`,
+  headers: {
+    Authorization: localStorage.getItem("token") || "",
+  },
+})
+
+export default authAPI
