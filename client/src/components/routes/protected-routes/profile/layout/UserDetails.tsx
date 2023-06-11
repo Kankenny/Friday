d07e26b87c2077a87c26d67f0eaf58d27f7db6b7
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-10 21:44:06 -0400

    feat: implement basic profile user details

--- client/src/components/routes/protected-routes/profile/layout/UserDetails.tsx
+++ client/src/components/routes/protected-routes/profile/layout/UserDetails.tsx
@@ -1,12 +1,33 @@
 import { useTypedSelector } from "../../../../../lib/hooks/redux-hook/useTypedSelector"
+import Card from "../../../../ui/Card"
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined"
 
 const UserDetails = () => {
-  const { username, firstName, lastName } = useTypedSelector(
-    (state) => state.profile
-  )
+  const { username, email, firstName, lastName, followers, following } =
+    useTypedSelector((state) => state.profile)
 
-  console.log(username, firstName, lastName)
-  return <div>UserDetails</div>
+  return (
+    <Card twClasses="p-5 flex flex-col gap-2">
+      <div className="pb-2 border-b border-secondary">
+        <h1 className="font-bold text-xl">
+          {firstName} {lastName}
+        </h1>
+        <h1>
+          @ <span className="underline">{username}</span>
+        </h1>
+        <h1 className="font-extralight">{email}</h1>
+      </div>
+      <div className="space-x-2">
+        <PeopleAltOutlinedIcon />{" "}
+        <span>
+          <b>{followers.length}</b> followers
+        </span>
+        <span>
+          <b>{following.length}</b> following
+        </span>
+      </div>
+    </Card>
+  )
 }
 
 export default UserDetails
