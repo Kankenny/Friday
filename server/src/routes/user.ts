37a3266bc39deba0d7fe58a3bdf7d564d2678bf4
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-11 18:16:28 -0400

    feat: declare and expose get user timeline on API

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -10,6 +10,7 @@ import {
   unfollowUser,
   blockUser,
   unblockUser,
+  getUserTimeline,
 } from "../controllers/user-controller/userController"
 
 // Types
@@ -34,6 +35,14 @@ UserRoute.get(
   (req: Request, res: Response, next: NextFunction) =>
     verifyToken(req as JWTRequest, res, next),
   (req: Request, res: Response) => getUserBlocked(req as JWTRequest, res)
+)
+
+// GET USER TIMELINE
+UserRoute.get(
+  "/timeline",
+  (req: Request, res: Response, next: NextFunction) =>
+    verifyToken(req as JWTRequest, res, next),
+  (req: Request, res: Response) => getUserTimeline(req as JWTRequest, res)
 )
 
 // FOLLOW USER
