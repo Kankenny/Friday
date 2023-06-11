ddd32384e51bfcd1c9bdabb8f9ac720ce4d5ff2b
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-10 21:55:31 -0400

    feat: add button to settings

--- client/src/components/routes/protected-routes/profile/layout/UserDetails.tsx
+++ client/src/components/routes/protected-routes/profile/layout/UserDetails.tsx
@@ -1,14 +1,21 @@
 import { useTypedSelector } from "../../../../../lib/hooks/redux-hook/useTypedSelector"
 import Card from "../../../../ui/Card"
 import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined"
+import StyledButton from "../../../../ui/StyledButton"
+import { useNavigate } from "react-router-dom"
 
 const UserDetails = () => {
   const { username, email, firstName, lastName, followers, following } =
     useTypedSelector((state) => state.profile)
+  const navigate = useNavigate()
+
+  const handleEditProfileClick = () => {
+    navigate("/settings")
+  }
 
   return (
     <Card twClasses="p-5 flex flex-col gap-2">
-      <div className="pb-2 border-b border-secondary">
+      <div className="pb-5 border-b border-secondary">
         <h1 className="font-bold text-xl">
           {firstName} {lastName}
         </h1>
@@ -16,7 +23,12 @@ const UserDetails = () => {
           <span className="text-xs">@</span>
           <span className="underline">{username}</span>
         </h1>
-        <h1 className="font-extralight">{email}</h1>
+        <h1 className="font-extralight text-sm pb-5">{email}</h1>
+        <StyledButton
+          onClick={handleEditProfileClick}
+          buttonText="Edit profile"
+          twClasses="w-full hover:scale-100 hover:bg-tertiary duration-200 hover:text-secondary border-2 border-secondary hover:border-secondary"
+        />
       </div>
       <div className="space-x-2">
         <PeopleAltOutlinedIcon />
--- client/src/components/ui/StyledButton.tsx
+++ client/src/components/ui/StyledButton.tsx
@@ -1,22 +1,28 @@
 type Props = {
-  onClick: () => void;
-  buttonText: string;
-  intent?: "primary" | "secondary";
-};
+  onClick: () => void
+  buttonText: string
+  intent?: "primary" | "secondary"
+  twClasses?: string
+}
 
-const StyledButton = ({ onClick, buttonText, intent = "primary" }: Props) => {
+const StyledButton = ({
+  onClick,
+  buttonText,
+  intent = "primary",
+  twClasses,
+}: Props) => {
   const activatedButtonClasses =
     intent === "primary"
       ? "bg-secondary text-main"
-      : "bg-main text-secondary border-2 border-secondary";
+      : "bg-main text-secondary border-2 border-secondary"
 
   return (
     <button
-      className={`w-28 rounded-xl text-center text-sm px-4 py-2 hover:scale-105 ease-in-out duration-100 ${activatedButtonClasses}`}
+      className={`w-28 rounded-xl text-center text-sm px-4 py-2 hover:scale-105 ease-in-out duration-100 ${activatedButtonClasses} ${twClasses}`}
       onClick={onClick}
     >
       {buttonText}
     </button>
-  );
-};
-export default StyledButton;
+  )
+}
+export default StyledButton
