2bc5b1d3ec97489a986db0ae70185f5dcf9a19d2
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-14 22:05:27 -0400

    refactor: customize detailed view of other user pfp

--- client/src/components/routes/protected-routes/profile/layout/other-user/profile-picture/PFPDialog.tsx
+++ client/src/components/routes/protected-routes/profile/layout/other-user/profile-picture/PFPDialog.tsx
@@ -10,18 +10,20 @@ type Props = {
 const PFPDialog = ({ firstName, profilePicture, open, onClose }: Props) => {
   return (
     <Dialog open={open} onClose={onClose}>
-      <div className="py-16 px-32 space-y-16 flex flex-col items-center">
-        {profilePicture ? (
-          <img
-            src={profilePicture}
-            alt="Profile Picture"
-            className="rounded-full object-cover w-full h-full"
-          />
-        ) : (
-          <h1 className="text-9xl text-gray-400 select-none capitalize">
-            {firstName.charAt(0)}
-          </h1>
-        )}
+      <div className="p-16">
+        <div className="w-96 h-96 flex justify-center items-center ">
+          {profilePicture ? (
+            <img
+              src={profilePicture}
+              alt="Profile Picture"
+              className="rounded-full object-cover w-full h-full"
+            />
+          ) : (
+            <h1 className="text-9xl bg-gray-400 text-main select-none capitalize w-full h-full rounded-full flex items-center justify-center">
+              {firstName.charAt(0)}
+            </h1>
+          )}
+        </div>
       </div>
     </Dialog>
   )
