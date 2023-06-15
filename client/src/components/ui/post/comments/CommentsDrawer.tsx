ce1c90ebc4b33cf18303923a44d7ab9e25d8021c
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-14 23:50:07 -0400

    feat: map over the comments

--- client/src/components/ui/post/comments/CommentsDrawer.tsx
+++ client/src/components/ui/post/comments/CommentsDrawer.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box"
 import { PostType } from "../../../../lib/types/primitive-types/PostType"
+import Comments from "./Comments"
 
 type Props = {
   post: PostType
@@ -15,9 +16,10 @@ const CommentsDrawer = ({ post, toggleDrawer }: Props) => {
       onKeyDown={toggleDrawer(false)}
       role="presentation"
     >
-      <h1 className="text-3xl font-bold text-tertiary">
+      <h1 className="text-3xl font-bold text-tertiary pb-5 border-b border-main mb-5">
         {post.title} — Comments
       </h1>
+      <Comments comments={post.comments} />
     </Box>
   )
 }
