f61affdd4e0f6963ea57c2ed8cc8fffb68318714
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-15 23:28:32 -0400

    refactor: set the scrollbar at the bottom most (most recent) by default at component mount

--- client/src/components/ui/post/comments/Comments.tsx
+++ client/src/components/ui/post/comments/Comments.tsx
@@ -1,15 +1,27 @@
 import { useTypedSelector } from "../../../../lib/hooks/redux-hook/useTypedSelector"
 import Comment from "./Comment"
+import { useRef, useEffect } from "react"
 
 const Comments = () => {
   const { comments } = useTypedSelector((state) => state.postDetail)
+  const containerRef = useRef<HTMLDivElement>(null)
 
   const content = comments.map((comment) => (
     <Comment key={comment._id} comment={comment} />
   ))
 
+  useEffect(() => {
+    const container = containerRef.current
+    if (container) {
+      container.scrollTop = container.scrollHeight
+    }
+  }, [comments])
+
   return (
-    <div className="space-y-5 overflow-y-scroll max-h-[50rem]">
+    <div
+      className="space-y-5 overflow-y-scroll max-h-[50rem]"
+      ref={containerRef}
+    >
       {comments.length !== 0 ? (
         content
       ) : (
