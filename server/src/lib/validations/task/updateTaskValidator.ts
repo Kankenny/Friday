e513601ddcc538b8c3b9cd0945ea1041ae2a17ac
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-15 22:34:18 -0400

    feat: check for priority and due date fields

--- server/src/lib/validations/task/updateTaskValidator.ts
+++ server/src/lib/validations/task/updateTaskValidator.ts
@@ -3,6 +3,8 @@ import { z } from "zod"
 const updateTaskSchema = z.object({
   title: z.string().optional(),
   progress: z.enum(["done", "working on it", "stuck", "untouched"]).optional(),
+  priority: z.enum(["low", "medium", "high"]).optional(),
+  dueDate: z.date().optional(),
 })
 
 export default updateTaskSchema
