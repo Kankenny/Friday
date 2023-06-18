6871561c969e936d8d364ed87d2a857121d83eef
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-17 23:18:50 -0400

    feat: implement task axios api instance

--- client/src/components/ui/post/PostActions.tsx
+++ client/src/components/ui/post/PostActions.tsx
@@ -1,28 +1,41 @@
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined"
-import { useRef } from "react"
 import { PostType } from "../../../lib/types/primitive-types/PostType"
 import CommentsButton from "./comments/CommentsButton"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  createTaskSchema,
+  createTaskType,
+} from "../../../../../common/validations/task/createTaskValidator"
+import { isAxiosError } from "axios"
 
 type Props = {
   post: PostType
 }
 
 const PostActions = ({ post }: Props) => {
-  const newTaskRef = useRef(null)
-
-  const handleNewTaskSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
+  const { register, handleSubmit } = useForm<createTaskType>({
+    resolver: zodResolver(createTaskSchema),
+  })
+  const handleNewTaskSubmit = async (formData: createTaskType) => {
+    try {
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   return (
     <div className="flex justify-between border border-secondary rounded-b-md text-sm p-2">
-      <form className="flex items-center" onSubmit={handleNewTaskSubmit}>
+      <form
+        className="flex items-center"
+        onSubmit={handleSubmit(handleNewTaskSubmit)}
+      >
         <AddOutlinedIcon className="h-5 w-5 opacity-50" />
         <input
           type="text"
           placeholder="Add Task"
           className="h-full outline-none"
-          ref={newTaskRef}
+          {...register("title")}
         />
       </form>
       <CommentsButton post={post} />
