e3112f91679dfff9985d68b583e1b3782067c18b
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-18 21:32:14 -0400

    feat: add styling:

--- client/src/components/ui/post/headers/ColumnHeaders.tsx
+++ client/src/components/ui/post/headers/ColumnHeaders.tsx
@@ -1,16 +1,16 @@
 const ColumnHeaders = () => {
   return (
     <div className="flex justify-between text-center font-semibold ">
-      <h1 className="flex-grow max-w-[50%] border border-secondary p-2 rounded-tl-md text-sm">
+      <h1 className="flex-grow max-w-[50%] bg-secondary text-tertiary border border-tertiary p-2 rounded-tl-md text-sm">
         Task
       </h1>
-      <h1 className="flex-grow max-w-[20%] border border-secondary p-2 text-sm">
+      <h1 className="flex-grow max-w-[20%] bg-secondary text-tertiary border border-tertiary p-2 text-sm">
         Progress
       </h1>
-      <h1 className="flex-grow max-w-[10%] border border-secondary p-2 text-sm">
+      <h1 className="flex-grow max-w-[10%] bg-secondary text-tertiary border border-tertiary p-2 text-sm">
         Priority
       </h1>
-      <h1 className="flex-grow max-w-[20%] border border-secondary p-2 rounded-tr-md text-sm">
+      <h1 className="flex-grow max-w-[20%] bg-secondary text-tertiary border border-tertiary p-2 rounded-tr-md text-sm">
         Due Date
       </h1>
     </div>
