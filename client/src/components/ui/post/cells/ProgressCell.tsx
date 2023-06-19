7dd1b152c39f284cf709b58b1c2f25db94b1a324
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-18 23:05:11 -0400

    feat: implement adaptive progress color

--- client/src/components/ui/post/cells/ProgressCell.tsx
+++ client/src/components/ui/post/cells/ProgressCell.tsx
@@ -1,10 +1,19 @@
 type Props = {
-  progress: string
+  progress: "done" | "working on it" | "stuck" | "untouched"
 }
 
 const ProgressCell = ({ progress }: Props) => {
+  const progressColor = {
+    done: "bg-[#499548] hover:bg-[#366136]",
+    "working on it": "bg-[#eab308] hover:bg-[#c79505]",
+    stuck: "bg-[#ef4444] hover:bg-[#be2d2d]",
+    untouched: "bg-gray-300 hover:bg-gray-400",
+  }
+
   return (
-    <h1 className="uppercase flex-grow max-w-[20%] border border-secondary p-2 text-sm cursor-pointer hover:bg-secondary hover:text-main duration-200">
+    <h1
+      className={`uppercase flex-grow max-w-[20%] border border-secondary p-2 text-sm cursor-pointer hover:text-secondary duration-200 ${progressColor[progress]}`}
+    >
       {progress}
     </h1>
   )
