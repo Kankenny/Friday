3a9f720eab66fa495121af7cf684386c5a4bc870
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-19 15:48:45 -0400

    refactor: abstract to NewSubtaskInput component

---
+++ client/src/components/ui/post/subtasks/NewSubtaskInput.tsx
@@ -0,0 +1,99 @@
+import { useEffect } from "react"
+import { TaskType } from "../../../../lib/types/primitive-types/TaskType"
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined"
+import { useDispatch } from "react-redux"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  createSubtaskSchema,
+  createSubtaskType,
+} from "../../../../../../common/validations/subtask/createSubTaskValidator"
+import subtaskAPI from "../../../../lib/services/axios-instances/subtaskAPI"
+import { PostType } from "../../../../lib/types/primitive-types/PostType"
+import { createSubtask } from "../../../../lib/store/slices/timeline-slice/timelineSlice"
+import { isAxiosError } from "axios"
+import { setFeedback } from "../../../../lib/store/slices/feedback-slice/feedbackSlice"
+
+type Props = {
+  post: PostType
+  task: TaskType
+}
+
+const NewSubtaskInput = ({ post, task }: Props) => {
+  const dispatch = useDispatch()
+
+  const {
+    register: registerNewSubtask,
+    handleSubmit: handleSubmitNewSubtask,
+    reset: resetNewSubtaskForm,
+    formState: {
+      errors: newSubtaskErrors,
+      isSubmitSuccessful: isNewSubtaskSubmittedSuccessful,
+    },
+  } = useForm<createSubtaskType>({
+    resolver: zodResolver(createSubtaskSchema),
+  })
+
+  const handleNewSubtaskSubmit = async (formData: createSubtaskType) => {
+    try {
+      const { data } = await subtaskAPI.post(
+        `/?taskId=${task._id}&postId=${post._id}`,
+        formData
+      )
+      dispatch(createSubtask({ post, task, subtask: data.data }))
+      dispatch(
+        setFeedback({
+          feedbackMessage: data.message,
+          feedbackType: "success",
+        })
+      )
+      resetNewSubtaskForm()
+    } catch (err) {
+      if (isAxiosError(err)) {
+        dispatch(
+          setFeedback({
+            feedbackMessage: err.response?.data.message,
+            feedbackType: "error",
+          })
+        )
+      }
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    if (newSubtaskErrors.title?.message && !isNewSubtaskSubmittedSuccessful) {
+      dispatch(
+        setFeedback({
+          feedbackMessage: newSubtaskErrors.title?.message,
+          feedbackType: "error",
+        })
+      )
+    }
+  }, [
+    newSubtaskErrors.title?.message,
+    isNewSubtaskSubmittedSuccessful,
+    dispatch,
+  ])
+
+  return (
+    <div className="border border-secondary p-2 pl-9 text-sm">
+      <form
+        className="flex items-center"
+        onSubmit={handleSubmitNewSubtask(handleNewSubtaskSubmit)}
+      >
+        <button type="submit">
+          <AddOutlinedIcon className="h-5 w-5 opacity-50" />
+        </button>
+        <input
+          type="text"
+          placeholder="Add Subtask"
+          className="bg-transparent px-2 h-full outline-none text-secondary rounded-md hover:border hover:border-secondary duration-200 ease-in-out"
+          {...registerNewSubtask("title")}
+        />
+      </form>
+    </div>
+  )
+}
+
+export default NewSubtaskInput
--- client/src/components/ui/post/tasks/Task.tsx
+++ client/src/components/ui/post/tasks/Task.tsx
@@ -1,23 +1,12 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import Subtasks from "../subtasks/Subtasks"
 import { TaskType } from "../../../../lib/types/primitive-types/TaskType"
-import AddOutlinedIcon from "@mui/icons-material/AddOutlined"
-import { useDispatch } from "react-redux"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import {
-  createSubtaskSchema,
-  createSubtaskType,
-} from "../../../../../../common/validations/subtask/createSubTaskValidator"
-import subtaskAPI from "../../../../lib/services/axios-instances/subtaskAPI"
 import { PostType } from "../../../../lib/types/primitive-types/PostType"
-import { createSubtask } from "../../../../lib/store/slices/timeline-slice/timelineSlice"
-import { isAxiosError } from "axios"
-import { setFeedback } from "../../../../lib/store/slices/feedback-slice/feedbackSlice"
 import ProgressCell from "../cells/ProgressCell"
 import PriorityCell from "../cells/PriorityCell"
 import DueDateCell from "../cells/DueDateCell"
 import TaskCell from "../cells/TaskCell"
+import NewSubtaskInput from "../subtasks/NewSubtaskInput"
 
 type Props = {
   post: PostType
@@ -25,62 +14,7 @@ type Props = {
 }
 
 const Task = ({ post, task }: Props) => {
-  const dispatch = useDispatch()
   const [isExpanded, setIsExpanded] = useState(false)
-
-  const {
-    register: registerNewSubtask,
-    handleSubmit: handleSubmitNewSubtask,
-    reset: resetNewSubtaskForm,
-    formState: {
-      errors: newSubtaskErrors,
-      isSubmitSuccessful: isNewSubtaskSubmittedSuccessful,
-    },
-  } = useForm<createSubtaskType>({
-    resolver: zodResolver(createSubtaskSchema),
-  })
-
-  const handleNewSubtaskSubmit = async (formData: createSubtaskType) => {
-    try {
-      const { data } = await subtaskAPI.post(
-        `/?taskId=${task._id}&postId=${post._id}`,
-        formData
-      )
-      dispatch(createSubtask({ post, task, subtask: data.data }))
-      dispatch(
-        setFeedback({
-          feedbackMessage: data.message,
-          feedbackType: "success",
-        })
-      )
-      resetNewSubtaskForm()
-    } catch (err) {
-      if (isAxiosError(err)) {
-        dispatch(
-          setFeedback({
-            feedbackMessage: err.response?.data.message,
-            feedbackType: "error",
-          })
-        )
-      }
-      console.error(err)
-    }
-  }
-
-  useEffect(() => {
-    if (newSubtaskErrors.title?.message && !isNewSubtaskSubmittedSuccessful) {
-      dispatch(
-        setFeedback({
-          feedbackMessage: newSubtaskErrors.title?.message,
-          feedbackType: "error",
-        })
-      )
-    }
-  }, [
-    newSubtaskErrors.title?.message,
-    isNewSubtaskSubmittedSuccessful,
-    dispatch,
-  ])
 
   const taskDueDate = new Date(task.dueDate)
   const formattedDueDate = taskDueDate.toLocaleString("en-US", {
@@ -120,24 +54,7 @@ const Task = ({ post, task }: Props) => {
       {isExpanded && task.subtasks.length !== 0 && (
         <Subtasks subtasks={task.subtasks} post={post} task={task} />
       )}
-      {isExpanded && (
-        <div className="border border-secondary p-2 pl-9 text-sm">
-          <form
-            className="flex items-center"
-            onSubmit={handleSubmitNewSubtask(handleNewSubtaskSubmit)}
-          >
-            <button type="submit">
-              <AddOutlinedIcon className="h-5 w-5 opacity-50" />
-            </button>
-            <input
-              type="text"
-              placeholder="Add Subtask"
-              className="bg-transparent px-2 h-full outline-none text-secondary rounded-md hover:border hover:border-secondary duration-200 ease-in-out"
-              {...registerNewSubtask("title")}
-            />
-          </form>
-        </div>
-      )}
+      {isExpanded && <NewSubtaskInput post={post} task={task} />}
     </>
   )
 }
