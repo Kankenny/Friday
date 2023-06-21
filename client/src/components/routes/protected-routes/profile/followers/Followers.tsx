3489202f99f40df1c615ec1c5305cb909062e149
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-21 19:52:31 -0400

    feat: conditionally render followers list depending on curr auth'd user

--- client/src/components/routes/protected-routes/profile/followers/Followers.tsx
+++ client/src/components/routes/protected-routes/profile/followers/Followers.tsx
@@ -2,13 +2,15 @@ import { useParams } from "react-router-dom"
 import { useTypedSelector } from "../../../../../lib/hooks/redux-hook/useTypedSelector"
 
 const Followers = () => {
-  const { username: currentUsername } = useTypedSelector(
-    (state) => state.sameProfile
+  const { username: currentUsername, followers: currentFollowers } =
+    useTypedSelector((state) => state.sameProfile)
+  const { followers: otherFollowers } = useTypedSelector(
+    (state) => state.otherProfile
   )
   const { username: pathUsername } = useParams()
   const isSameUser = currentUsername === pathUsername
 
-  const followers = isSameUser ? <div>T</div> : <div>K</div>
+  const followers = isSameUser ? currentFollowers : otherFollowers
   return <div>{followers}</div>
 }
 
