d4bd6dc406f55f6f3c5449b728c42006f1a6f855
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-21 21:24:23 -0400

    feat: implement block action component

---
+++ client/src/components/ui/user/BlockAction.tsx
@@ -0,0 +1,62 @@
+import { isAxiosError } from "axios"
+import { useDispatch } from "react-redux"
+import { useTypedSelector } from "../../../lib/hooks/redux-hook/useTypedSelector"
+import userAPI from "../../../lib/services/axios-instances/userAPI"
+import { setFeedback } from "../../../lib/store/slices/feedback-slice/feedbackSlice"
+import StyledButton from "../StyledButton"
+import { UserType } from "../../../lib/types/primitive-types/UserType"
+import {
+  followUser,
+  unfollowUser,
+} from "../../../lib/store/slices/same-profile-slice/sameProfileSlice"
+import { ProfileSliceType } from "../../../lib/types/slice-types/ProfileSliceType"
+
+type Props = {
+  user: UserType | ProfileSliceType
+}
+
+const BlockAction = ({ user }: Props) => {
+  const {
+    _id: currentId,
+    username: currentUsername,
+    blocked: currBlocked,
+  } = useTypedSelector((state) => state.sameProfile)
+  const isSameUser = currentUsername === user.username
+
+  const isAlreadyBlocked = currBlocked.includes(user)
+  const buttonText = isAlreadyBlocked ? "Blocked" : "block"
+
+  const dispatch = useDispatch()
+  const handleBlockOrUnblockClick = async () => {
+    try {
+      const action = isAlreadyBlocked ? "unblock" : "block"
+      const { data } = await userAPI.put(`/${currentId}/${action}/${user._id}`)
+      dispatch(
+        setFeedback({
+          feedbackMessage: data.message,
+          feedbackType: "success",
+        })
+      )
+      if (isAlreadyBlocked) {
+        dispatch(unfollowUser(user))
+      } else {
+        dispatch(followUser(user))
+      }
+    } catch (err) {
+      if (isAxiosError(err)) {
+        dispatch(
+          setFeedback({
+            feedbackMessage: err.response?.data.message,
+            feedbackType: "error",
+          })
+        )
+      }
+    }
+  }
+
+  return !isSameUser ? (
+    <StyledButton buttonText={buttonText} onClick={handleBlockOrUnblockClick} />
+  ) : null
+}
+
+export default BlockAction
