fbba760e607d67516611747c85a8c1e044e21159
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-22 16:58:35 -0400

    refactor: populate authorized users field

--- client/src/lib/store/slices/post-detail-slice/postDetailActions.ts
+++ client/src/lib/store/slices/post-detail-slice/postDetailActions.ts
@@ -7,7 +7,6 @@ export const setPostDetailReducer = (
   state: PostDetailSliceType,
   action: PayloadAction<PostDetailSliceType[]>
 ) => {
-  console.log(action.payload)
   return { ...state, ...action.payload }
 }
 
