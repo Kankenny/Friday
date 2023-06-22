d05c266fb21b24d20f345a2819d2207267442c53
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-22 15:27:47 -0400

    feat: implement basic auth users dialog

--- client/src/components/ui/post/headers/post-menu/AuthorizedUsersDialog.tsx
+++ client/src/components/ui/post/headers/post-menu/AuthorizedUsersDialog.tsx
@@ -1,5 +1,7 @@
 import Dialog from "@mui/material/Dialog"
 import { PostType } from "../../../../../lib/types/primitive-types/PostType"
+import UserCard from "../../../user/UserCard"
+import Alert from "../../../mui/Alert"
 
 type Props = {
   post: PostType
@@ -8,13 +10,28 @@ type Props = {
 }
 
 const AuthorizedUsersDialog = ({ post, open, onClose }: Props) => {
+  const authorizedUsers = post.authorizedUsers
+
+  const content =
+    authorizedUsers.length !== 0 ? (
+      authorizedUsers.map((user) => <UserCard user={user} key={user._id} />)
+    ) : (
+      <Alert
+        message="No users are currently authorized except you"
+        severity="error"
+      />
+    )
+
   return (
     <Dialog open={open} onClose={onClose}>
-      <div className="py-16 px-32 space-y-16 flex flex-col items-center">
-        <div className="text-3xl font-bold text-secondary text-center">
-          <h1>Set Profile Picture</h1>
-          <p className="text-sm font-light">Select or drag an image here</p>
+      <div className="p-16 gap-5 flex flex-col items-center max-h-96">
+        <div className="text-3xl font-bold text-secondary text-center w-full">
+          <h1>Authorized Users</h1>
+          <p className="text-sm font-light">
+            Manage who has collaboration access to this post
+          </p>
         </div>
+        {content}
       </div>
     </Dialog>
   )
