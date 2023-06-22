8e694311b04bec5f8de6efead80c7be12617775a
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-21 22:16:22 -0400

    refactor: add comment

--- client/src/lib/store/slices/other-profile-slice/otherProfileActions.ts
+++ client/src/lib/store/slices/other-profile-slice/otherProfileActions.ts
@@ -16,5 +16,7 @@ export const followedUserReducer = (
 }
 
 export const unfollowedUserReducer = (state: ProfileSliceType) => {
+  // hacky way to reduce number of followers
+  // note: we only use this to display the NUMBER of followers of the other user
   state.followers = state.followers.slice(1)
 }
