481ad76fb0fe835a733f638883dc2ff8fb1f3c3f
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-21 20:36:23 -0400

    feat: implement follow or unfollow button

--- client/src/components/routes/protected-routes/profile/followers/Follower.tsx
+++ client/src/components/routes/protected-routes/profile/followers/Follower.tsx
@@ -1,6 +1,7 @@
 import { UserType } from "../../../../../lib/types/primitive-types/UserType"
 import Card from "../../../../ui/Card"
 import Avatar from "@mui/material/Avatar"
+import FollowerAction from "./FollowerAction"
 
 type Props = {
   follower: UserType
@@ -10,19 +11,22 @@ const Follower = ({ follower }: Props) => {
   const { firstName, lastName, profilePicture, username } = follower
 
   return (
-    <Card twClasses="border border-secondary bg-blue-200 group shadow-md p-2 md:p-4 flex items-center gap-2 md:gap-4">
-      <Avatar
-        className="text-secondary capitalize cursor-pointer"
-        src={profilePicture}
-      >
-        {profilePicture ? "" : firstName.charAt(0)}
-      </Avatar>
-      <div className="group-hover:text-tertiary duration-200 ease-in-out cursor-pointer">
-        <h1 className="group-hover:underline">
-          {firstName} {lastName}
-        </h1>
-        <p className="text-xs font-light">@{username}</p>
+    <Card twClasses="flex items-center justify-between border border-secondary bg-blue-200 shadow-md p-2 md:p-4">
+      <div className="group flex items-center gap-2 md:gap-4">
+        <Avatar
+          className="text-secondary capitalize cursor-pointer"
+          src={profilePicture}
+        >
+          {profilePicture ? "" : firstName.charAt(0)}
+        </Avatar>
+        <div className="group-hover:text-tertiary duration-200 ease-in-out cursor-pointer">
+          <h1 className="group-hover:underline">
+            {firstName} {lastName}
+          </h1>
+          <p className="text-xs font-light text-gray-600">@ {username}</p>
+        </div>
       </div>
+      <FollowerAction follower={follower} />
     </Card>
   )
 }
