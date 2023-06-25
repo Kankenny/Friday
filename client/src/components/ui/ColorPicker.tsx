6a6c0a2488ee0bc6e9c44f2003b6198b3c653178
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-25 17:21:25 -0400

    feat: implement color picker

--- client/src/components/ui/ColorPicker.tsx
+++ client/src/components/ui/ColorPicker.tsx
@@ -1,1 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { TwitterPicker } from "react-color"
+import { useState } from "react"
+
+const ColorPicker = () => {
+  const [color, setColor] = useState("fff")
+
+  const handleColorChange = (color: any) => {
+    setColor(color)
+  }
+
+  return <TwitterPicker onChange={handleColorChange} color={color} />
+}
+
+export default ColorPicker
