8454e6b7714fbf81343130c455fa4b18749a1156
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-25 17:30:11 -0400

    feat: conditionally render color picker component

--- client/src/components/ui/post/headers/ColorCode.tsx
+++ client/src/components/ui/post/headers/ColorCode.tsx
@@ -1,5 +1,14 @@
+import ColorPicker from "../../ColorPicker"
+import { useState } from "react"
+
 const ColorCode = () => {
-  return <div className="bg-red-500 h-2 rounded-t-sm"></div>
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className="bg-red-500 h-2 rounded-t-sm" onClick={() => setOpen(!open)}>
+      {open && <ColorPicker />}
+    </div>
+  )
 }
 
 export default ColorCode
