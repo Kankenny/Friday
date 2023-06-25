dc73ea1f4a6e2b7608bd991f5a934143f862bb37
Kennette James Basco <kenji.maddela01@gmail.com>
2023-06-25 17:36:29 -0400

    refactor: utilize popper

--- client/src/components/ui/post/headers/ColorCode.tsx
+++ client/src/components/ui/post/headers/ColorCode.tsx
@@ -1,12 +1,20 @@
 import ColorPicker from "../../ColorPicker"
-import { useState } from "react"
+import { useState, useRef } from "react"
+import Popper from "@mui/material/Popper"
 
 const ColorCode = () => {
   const [open, setOpen] = useState(false)
+  const anchorRef = useRef<HTMLDivElement>(null)
 
   return (
-    <div className="bg-red-500 h-2 rounded-t-sm" onClick={() => setOpen(!open)}>
-      {open && <ColorPicker />}
+    <div
+      className="bg-red-500 h-2 rounded-t-sm"
+      onClick={() => setOpen(!open)}
+      ref={anchorRef}
+    >
+      <Popper open={open} anchorEl={anchorRef.current}>
+        <ColorPicker />
+      </Popper>
     </div>
   )
 }
