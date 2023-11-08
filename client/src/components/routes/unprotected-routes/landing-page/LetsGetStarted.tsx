2c8692646279ed6d8d757218ddf3626191ca9893
THossen <tan.hossen.1@gmail.com>
2023-11-08 18:02:46 -0500

    refactor: routing fixes to LetsGetStarted component

--- client/src/components/routes/unprotected-routes/landing-page/LetsGetStarted.tsx
+++ client/src/components/routes/unprotected-routes/landing-page/LetsGetStarted.tsx
@@ -6,7 +6,7 @@ const LetsGetStarted = () => {
   return (
     <div className="bg-secondary align-center flex justify-center border-b border-b-white p-32">
       <Link
-        to="/app"
+        to="/register"
         className="bg-tertiary text-primary text-main hover:text-secondary rounded-md px-10 py-4 text-3xl font-bold duration-200 ease-in-out hover:scale-105"
       >
         Let's Get Started
